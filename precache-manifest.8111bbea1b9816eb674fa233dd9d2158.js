dd90b7eaaefd86ffbd6a1627045b21ec00b64b6f
MonteCristo <mjblee20@gmail.com>
2020-08-21 16:05:50 -0700

    Updates

---
+++ precache-manifest.8111bbea1b9816eb674fa233dd9d2158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7dcfa32d9d4ccd5c8be1fc10d533a6",
+    "url": "/sample-react/index.html"
+  },
+  {
+    "revision": "f02a58001e2da6ff906d",
+    "url": "/sample-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "530bdaffeb20951178ff",
+    "url": "/sample-react/static/js/2.05797747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-react/static/js/2.05797747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02a58001e2da6ff906d",
+    "url": "/sample-react/static/js/main.00d6680a.chunk.js"
+  },
+  {
+    "revision": "62ded73261be4bd990ee",
+    "url": "/sample-react/static/js/runtime-main.472a6a31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
