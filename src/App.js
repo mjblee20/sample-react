e527056c59e808f44f08bc87829569f122b9ad62
MonteCristo <mjblee20@gmail.com>
2020-08-22 11:43:51 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-import './App.css';
 
 function App() {
   return (
